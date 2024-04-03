0a55a34f8bfcbad6eb5e6d07ff15ae5ceb13c02d
Aakashraz <aakashraz@outlook.com>
2024-04-04 01:09:14 +0545

    work on get and post function

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
@@ -32,6 +33,9 @@ func connect() error {
 	defer cancel()
 
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+	//client: This variable holds a pointer to a mongo.Client object, which represents the connection to the MongoDB server.
+	//You can use this client object to perform database operations such as querying and inserting documents.
+
 	if err != nil {
 		log.Printf("error while connecting to mongoDB: %s", err)
 		return err
@@ -53,10 +57,45 @@ func main() {
 		log.Fatal(err)
 	}
 	app := fiber.New()
-	app.Get("/employee", func(ctx *fiber.Ctx) error {
 
+	app.Get("/employee", func(ctx *fiber.Ctx) error {
+		query := bson.D{{}}
+		var employees []Employee = make([]Employee, 0)
+		cursor, err := mg.Db.Collection("employees").Find(ctx.Context(), query)
+		if err != nil {
+			return ctx.Status(500).SendString(err.Error())
+		}
+
+		if err := cursor.All(ctx.Context(), &employees); err != nil {
+			return ctx.Status(500).SendString(err.Error())
+		}
+		return ctx.JSON(employees)
 	})
-	app.Post("/employee")
+
+	app.Post("/employee", func(c *fiber.Ctx) error {
+		collection := mg.Db.Collection("employees")
+		employee := new(Employee)
+		//	employee := ...: The result of new(Employee) is assigned to the variable employee.
+		//	Since new returns a pointer to the allocated memory, employee is of type *Employee, which is a pointer to an Employee struct.
+
+		employee.ID = ""
+		insertionResult, err := collection.InsertOne(c.Context(), employee)
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+
+		// to ensure that the data is inserted, we will be using the inserted_ID to find and return that exact data
+		filter := bson.D{{Key: "_id", Value: insertionResult.InsertedID}}
+		createdRecord := collection.FindOne(c.Context(), filter)
+
+		createdEmployee := &Employee{}
+		err = createdRecord.Decode(createdEmployee)
+		if err != nil {
+			return err
+		}
+
+		return c.Status(201).JSON(createdEmployee)
+	})
 	app.Put("/employee/:id")
 	app.Delete("/employee/:id")
 }
